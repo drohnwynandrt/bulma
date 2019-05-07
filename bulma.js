82e540c1da6ca9e8d206362cd035ee03aa15c906
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-07 14:45:56 +0200

    Added a bulma template to my project

---
+++ bulma.js
@@ -0,0 +1,9 @@
+var word = "test";
+var letter = ["t", "e", "s", "d"]
+
+function checkWord(){
+    if (document.getElementById("guess").value== word){
+    alert("Goedzo!");}
+    else {
+        alert("Bijna goed!")}
+}
